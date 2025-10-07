8742ae31dacdc32a8486835fdb1d386a45ae2bb3
Ebrahimakbari <y560mia3@gmail.com>
2025-10-07 21:50:03 +0330

    feat: change on models and tasks and add admin

--- mobiles/admin.py
+++ mobiles/admin.py
@@ -1,195 +1,164 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import (
-    Brand, Category, Color, Warranty, Seller,
-    MobileImage, Specification, Variant, Mobile
-)
+from django.db.models import Prefetch
+import json
+from .models import Brand, Category, Mobile, Variant, MobileImage
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ('title_fa', 'title_en', 'code', 'is_premium', 'visibility')
-    list_filter = ('is_premium', 'visibility', 'created_at')
-    search_fields = ('title_fa', 'title_en', 'code')
+    list_display = ('title_en', 'title_fa', 'code', 'created_at')
+    search_fields = ('title_en', 'title_fa', 'code')
+    list_filter = ('created_at',)
+    ordering = ('title_en',)
     readonly_fields = ('api_id', 'created_at', 'updated_at')
-    ordering = ('title_fa',)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title_fa', 'title_en', 'code')
+    list_display = ('title_fa', 'title_en', 'code', 'created_at')
     search_fields = ('title_fa', 'title_en', 'code')
-    readonly_fields = ('api_id', 'created_at', 'updated_at')
+    list_filter = ('created_at',)
     ordering = ('title_fa',)
-
-@admin.register(Color)
-class ColorAdmin(admin.ModelAdmin):
-    list_display = ('title', 'title_fa', 'title_en', 'hex_code_display')
-    search_fields = ('title', 'title_fa', 'title_en')
-    readonly_fields = ('api_id', 'created_at')
-    
-    def hex_code_display(self, obj):
-        if obj.hex_code:
-            return format_html(
-                '<span style="background-color: {}; padding: 5px;">{}</span>',
-                obj.hex_code,
-                obj.hex_code
-            )
-        return '-'
-    hex_code_display.short_description = 'Color'
-
-@admin.register(Warranty)
-class WarrantyAdmin(admin.ModelAdmin):
-    list_display = ('title_fa', 'title_en')
-    search_fields = ('title_fa', 'title_en')
-    readonly_fields = ('api_id', 'created_at')
-    ordering = ('title_fa',)
-
-@admin.register(Seller)
-class SellerAdmin(admin.ModelAdmin):
-    list_display = ('title', 'title_fa', 'code')
-    search_fields = ('title', 'title_fa', 'code')
     readonly_fields = ('api_id', 'created_at', 'updated_at')
-    ordering = ('title',)
-
-class MobileImageInline(admin.TabularInline):
-    model = MobileImage
-    extra = 1
-    readonly_fields = ('id', 'created_at')
-    fields = ('url', 'thumbnail_url', 'webp_url', 'is_main')
-
-class SpecificationInline(admin.TabularInline):
-    model = Specification
-    extra = 1
-    readonly_fields = ('created_at',)
-    fields = ('title', 'title_fa', 'value', 'value_fa')
 
 class VariantInline(admin.TabularInline):
     model = Variant
-    extra = 1
-    readonly_fields = ('api_id', 'created_at', 'updated_at')
-    fields = (
-        'color', 'warranty', 'seller',
-        'selling_price', 'rrp_price',
-        'availability', 'is_incredible'
-    )
+    extra = 0
+    fields = ('api_id', 'seller_name', 'color_preview', 'color_name', 'color_hex',
+              'warranty_name', 'selling_price', 'rrp_price', 'order_limit', 'is_incredible')
+    readonly_fields = ('api_id', 'color_preview')
+    
+    def color_preview(self, obj):
+        if obj.color_hex:
+            return format_html(
+                '<div style="width: 30px; height: 30px; background-color: {}; '
+                'border: 1px solid #ccc; border-radius: 3px;"></div>',
+                obj.color_hex
+            )
+        return "No Color"
+    color_preview.short_description = 'Color'
+
+class MobileImageInline(admin.TabularInline):
+    model = MobileImage
+    extra = 0
+    fields = ('image_preview', 'is_main')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image_url:
+            return format_html(
+                '<img src="{}" style="width: 45px; height: 45px; object-fit: cover;" />',
+                obj.image_url
+            )
+        return "No Image"
+    image_preview.short_description = 'Image'
 
 @admin.register(Mobile)
 class MobileAdmin(admin.ModelAdmin):
-    list_display = (
-        'title_fa', 'brand', 'category',
-        'rating_rate', 'rating_count',
-        'average_price', 'status'
-    )
-    list_filter = (
-        'status', 'brand', 'category',
-        'is_jet_eligible', 'created_at'
-    )
-    search_fields = (
-        'title_fa', 'title_en',
-        'brand__title_fa', 'category__title_fa'
-    )
-    readonly_fields = (
-        'api_id', 'slug', 'created_at',
-        'updated_at', 'api_fetched_at'
-    )
-    inlines = [
-        MobileImageInline,
-        SpecificationInline,
-        VariantInline
-    ]
+    list_display = ('title_en', 'title_fa', 'brand', 'category', 'status', 
+                   'rating_rate', 'rating_count', 'main_image_preview', 'created_at')
+    search_fields = ('title_en', 'title_fa', 'brand__title_en', 'brand__title_fa')
+    list_filter = ('status', 'brand', 'category', 'created_at')
+    ordering = ('-created_at',)
+    readonly_fields = ('api_id', 'slug', 'created_at', 'updated_at', 
+                      'formatted_review', 'formatted_specs')
+    inlines = [VariantInline, MobileImageInline] 
     
     fieldsets = (
         ('Basic Information', {
-            'fields': (
-                'title_fa', 'title_en', 'slug',
-                'url', 'status', 'product_type'
-            )
-        }),
-        ('Relations', {
-            'fields': ('brand', 'category')
-        }),
-        ('Description', {
-            'fields': (
-                'description', 'test_title_fa',
-                'test_title_en', 'alternate_name'
-            )
-        }),
-        ('Metrics', {
-            'fields': (
-                'metric6', 'dimension2', 'dimension6',
-                'dimension7', 'dimension9', 'dimension11', 'dimension20'
-            )
+            'fields': ('api_id', 'slug', 'title_fa', 'title_en', 'status', 'brand', 'category')
         }),
         ('Rating', {
-            'fields': (
-                'rating_rate', 'rating_count',
-                'review_rating_value', 'review_author_name',
-                'review_date_published', 'review_body'
-            )
+            'fields': ('rating_rate', 'rating_count')
         }),
-        ('Services', {
-            'fields': (
-                'digiplus_services', 'digiplus_service_list',
-                'digiplus_services_summary', 'is_jet_eligible',
-                'is_general_location_jet_eligible', 'fast_shipping_text'
-            )
-        }),
-        ('Identifiers', {
-            'fields': ('mpn', 'sku', 'price_currency')
-        }),
-        ('Timestamps', {
-            'fields': (
-                'created_at', 'updated_at', 'api_fetched_at'
-            ),
+        ('Review Data', {
+            'fields': ('formatted_review',),
             'classes': ('collapse',)
         }),
+        ('Specifications', {
+            'fields': ('formatted_specs',),
+            'classes': ('collapse',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        })
     )
-    
-    def average_price(self, obj):
-        price = obj.average_price
-        if price:
-            return f"{price:,} IRR"
-        return '-'
-    average_price.short_description = 'Lowest Price'
 
-@admin.register(MobileImage)
-class MobileImageAdmin(admin.ModelAdmin):
-    list_display = ('mobile', 'is_main', 'url_preview')
-    list_filter = ('is_main', 'created_at')
-    readonly_fields = ('id', 'created_at')
-    
-    def url_preview(self, obj):
-        if obj.url:
+    def formatted_review(self, obj):
+        if obj.review:
+            formatted_json = json.dumps(obj.review, indent=2, ensure_ascii=False)
             return format_html(
-                '<img src="{}" style="width: 50px; height: 50px;" />',
-                obj.url
+                '<pre style="white-space: pre-wrap; word-wrap: break-word;">{}</pre>',
+                formatted_json
             )
-        return '-'
-    url_preview.short_description = 'Preview'
+        return "No Review Data"
+    formatted_review.short_description = 'Review Details'
 
-@admin.register(Specification)
-class SpecificationAdmin(admin.ModelAdmin):
-    list_display = ('mobile', 'title', 'value')
-    search_fields = ('title', 'value', 'mobile__title_fa')
-    readonly_fields = ('created_at',)
+    def formatted_specs(self, obj):
+        if obj.specifications:
+            formatted_json = json.dumps(obj.specifications, indent=2, ensure_ascii=False)
+            return format_html(
+                '<pre style="white-space: pre-wrap; word-wrap: break-word;">{}</pre>',
+                formatted_json
+            )
+        return "No Specification Data"
+    formatted_specs.short_description = 'Specifications'
+
+    def main_image_preview(self, obj):
+        main_image = obj.images.filter(is_main=True).first()
+        if main_image and main_image.image_url:
+            return format_html(
+                '<img src="{}" style="width: 45px; height: 45px; object-fit: cover;" />',
+                main_image.image_url
+            )
+        return "No Main Image"
+    main_image_preview.short_description = 'Main Image'
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('brand', 'category').prefetch_related(
+            Prefetch('variants', queryset=Variant.objects.all().order_by('-selling_price')),
+            'images'
+        )
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        obj = self.get_object(request, object_id)
+        if obj:
+            extra_context['variants_count'] = obj.variants.count()
+            extra_context['images_count'] = obj.images.count()
+        return super().change_view(request, object_id, form_url, extra_context)
 
 @admin.register(Variant)
 class VariantAdmin(admin.ModelAdmin):
-    list_display = (
-        'mobile', 'color', 'warranty',
-        'seller', 'selling_price', 'availability'
-    )
-    list_filter = (
-        'color', 'warranty', 'seller',
-        'is_incredible', 'availability'
-    )
-    search_fields = (
-        'mobile__title_fa', 'color__title',
-        'seller__title'
-    )
-    readonly_fields = (
-        'api_id', 'created_at', 'updated_at'
-    )
-    
-    def selling_price_display(self, obj):
-        return f"{obj.selling_price:,} IRR"
-    selling_price_display.short_description = 'Price'
+    list_display = ('mobile', 'seller_name', 'color_preview', 'color_name', 
+                   'warranty_name', 'selling_price', 'rrp_price', 'is_incredible')
+    search_fields = ('mobile__title_en', 'mobile__title_fa', 'seller_name', 'color_name')
+    list_filter = ('is_incredible', 'warranty_name')
+    ordering = ('-selling_price',)
+    readonly_fields = ('api_id',)
+
+    def color_preview(self, obj):
+        if obj.color_hex:
+            return format_html(
+                '<div style="width: 30px; height: 30px; background-color: {}; '
+                'border: 1px solid #ccc; border-radius: 3px;"></div>',
+                obj.color_hex
+            )
+        return "No Color"
+    color_preview.short_description = 'Color'
+
+@admin.register(MobileImage)
+class MobileImageAdmin(admin.ModelAdmin):
+    list_display = ('mobile', 'image_preview', 'is_main')
+    search_fields = ('mobile__title_en', 'mobile__title_fa')
+    list_filter = ('is_main',)
+    ordering = ('-is_main',)
+
+    def image_preview(self, obj):
+        if obj.image_url:
+            return format_html(
+                '<img src="{}" style="width: 45px; height: 45px; object-fit: cover;" />',
+                obj.image_url
+            )
+        return "No Image"
+    image_preview.short_description = 'Image Preview'
---
+++ mobiles/tasks.py
@@ -0,0 +1,153 @@
+import requests
+from celery import shared_task
+from django.db import transaction, IntegrityError
+from django.utils import timezone
+import logging
+from decouple import config
+from fake_useragent import UserAgent
+
+from .models import Mobile, Brand, Category, MobileImage, Variant
+
+# --- Configuration ---
+LIST_API_URL_MOBILE = config('LIST_API_URL_MOBILE')
+DETAIL_API_URL_MOBILE = config('DETAIL_API_URL_MOBILE')
+logger = logging.getLogger(__name__)
+
+# Use a session for performance
+ua = UserAgent()
+session = requests.Session()
+session.headers.update({
+    'User-Agent': ua.random,
+    'Accept': 'application/json, text/plain, */*',
+    'Referer': 'https://www.digikala.com/',
+})
+
+@shared_task(bind=True, max_retries=3, default_retry_delay=60, autoretry_for=(requests.exceptions.RequestException,))
+def fetch_and_save_mobile_details(self, product_id):
+    """
+    Fetches and saves focused product information, leveraging bulk operations.
+    """
+    url = DETAIL_API_URL_MOBILE.format(product_id=product_id)
+    logger.info(f"Processing product ID: {product_id}")
+    
+    try:
+        response = session.get(url, timeout=20)
+        response.raise_for_status()
+        data = response.json().get('data', {})
+        product_data = data.get('product')
+
+        if not (product_data and product_data.get('id')):
+            return f"Skipped {product_id}: No product data."
+
+        brand_data = product_data.get('brand')
+        category_data = product_data.get('category')
+        if not (brand_data and brand_data.get('id') and category_data and category_data.get('id')):
+            return f"Skipped {product_id}: Missing brand or category."
+
+        with transaction.atomic():
+            brand, _ = Brand.objects.update_or_create(
+                api_id=brand_data['id'],
+                defaults={'code': brand_data.get('code', ''), 'title_fa': brand_data.get('title_fa', ''), 'title_en': brand_data.get('title_en', '')}
+            )
+            category, _ = Category.objects.update_or_create(
+                api_id=category_data['id'],
+                defaults={'code': category_data.get('code', ''), 'title_fa': category_data.get('title_fa', ''), 'title_en': category_data.get('title_en', '')}
+            )
+
+            mobile, created = Mobile.objects.update_or_create(
+                api_id=product_data['id'],
+                defaults={
+                    'title_fa': product_data.get('title_fa'),
+                    'title_en': product_data.get('title_en'),
+                    'brand': brand,
+                    'category': category,
+                    'status': product_data.get('status', 'unavailable'),
+                    'rating_rate': product_data.get('rating', {}).get('rate', 0),
+                    'rating_count': product_data.get('rating', {}).get('count', 0),
+                    # Directly save the entire 'review' and 'specifications' JSON objects
+                    'review': product_data.get('review', {}),
+                    'specifications': product_data.get('specifications', [])
+                }
+            )
+
+            # --- Bulk Create Images ---
+            MobileImage.objects.filter(mobile=mobile).delete()
+            images_to_create = []
+            main_image_url = product_data.get('images', {}).get('main', {}).get('url', [None])[0]
+            for img_data in product_data.get('images', {}).get('list', []):
+                if (urls := img_data.get('url')) and isinstance(urls, list) and urls:
+                    images_to_create.append(MobileImage(
+                        mobile=mobile, image_url=urls[0], is_main=(urls[0] == main_image_url)
+                    ))
+            if images_to_create:
+                MobileImage.objects.bulk_create(images_to_create)
+
+            # --- Bulk Create Variants ---
+            Variant.objects.filter(mobile=mobile).delete()
+            variants_to_create = []
+            for variant_data in product_data.get('variants', []):
+                if not (variant_id := variant_data.get('id')):
+                    continue
+                
+                price_info = variant_data.get('price', {})
+                variants_to_create.append(Variant(
+                    api_id=variant_id, mobile=mobile,
+                    seller_name=variant_data.get('seller', {}).get('title', ''),
+                    color_name=variant_data.get('color', {}).get('title', ''),
+                    color_hex=variant_data.get('color', {}).get('hex_code', ''),
+                    warranty_name=variant_data.get('warranty', {}).get('title_fa', ''),
+                    selling_price=price_info.get('selling_price', 0) // 10,
+                    rrp_price=price_info.get('rrp_price', 0) // 10,
+                    order_limit=price_info.get('order_limit', 1),
+                    is_incredible=price_info.get('is_incredible', False)
+                ))
+            if variants_to_create:
+                Variant.objects.bulk_create(variants_to_create)
+
+        status_msg = "Created" if created else "Updated"
+        logger.info(f"Successfully {status_msg} product ID: {product_id}")
+        return f"Success: {status_msg} {product_id}"
+
+    except IntegrityError as e:
+        logger.error(f"Database integrity error for product ID {product_id}: {e}. Skipping.")
+        return f"Failed {product_id}: IntegrityError"
+    except Exception as e:
+        logger.exception(f"CRITICAL UNEXPECTED ERROR for product ID {product_id}: {e}")
+        # Reraise to let Celery handle the retry logic
+        raise self.retry(exc=e)
+
+
+@shared_task
+def scrape_mobile_products(start_page=1, max_pages=20):
+    """
+    Scrapes mobile products from the list API and queues detail tasks.
+    """
+    logger.info(f"Starting mobile product scrape from page {start_page} for {max_pages} pages.")
+    processed_count = 0
+    for page_num in range(start_page, start_page + max_pages):
+        try:
+            url = LIST_API_URL_MOBILE.format(page=page_num)
+            response = session.get(url, timeout=15)
+            response.raise_for_status()
+            api_data = response.json().get('data', {})
+            products = api_data.get('products', [])
+            if not products:
+                logger.info(f"No more products found on page {page_num}. Stopping scrape.")
+                break
+
+            for product in products:
+                product_id = product.get('id')
+                if product_id and not Mobile.objects.filter(api_id=product_id).exists():
+                    fetch_and_save_mobile_details.delay(product_id)
+                    processed_count += 1
+
+            logger.info(f"Queued tasks for {len([p for p in products if p.get('id')])} new products from page {page_num}.")
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Network error on page {page_num}: {e}")
+            continue
+        except Exception as e:
+            logger.exception(f"An unexpected error on page {page_num}: {e}")
+            continue
+
+    logger.info(f"Mobile product scraping task finished. Queued {processed_count} new products.")
+    return f"Scraping completed: {processed_count} products queued."
